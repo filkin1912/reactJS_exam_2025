71bbc297d95ef042a5368c691d6edb674cadf57e
Filkin1912 <filkin1912@abv.bg>
2025-03-31 00:16:53 +0300

    Added BoughtGames and RouteGuards functionality

---
+++ services/gameService.js
@@ -0,0 +1,53 @@
+import {requestFactory} from './requester';
+import * as commentService from '../services/commentService';
+
+const baseUrl = 'http://localhost:3030/data/games';
+
+export const gameServiceFactory = (token) => {
+    const request = requestFactory(token);
+    const requestWithoutToken = requestFactory();
+
+    const getAll = async () => {
+        const result = await requestWithoutToken.get(baseUrl);
+        const games = Object.values(result);
+
+        return games;
+    };
+
+    const getOne = async (gameId) => {
+        const result = await request.get(`${baseUrl}/${gameId}`);
+        // add this check
+        if (!result.comments) {
+            result.comments = [];
+        }
+        return result;
+    };
+
+    const create = async (gameData) => {
+        const result = await request.post(baseUrl, gameData);
+
+        console.log(result);
+
+        return result;
+    };
+
+    const addComment = async (data) => {
+        const result = commentService.create(data);
+        console.log(result)
+        return result;
+    };
+
+    const edit = (gameId, data) => request.put(`${baseUrl}/${gameId}`, data);
+
+    const deleteGame = (gameId) => request.delete(`${baseUrl}/${gameId}`);
+
+
+    return {
+        getAll,
+        getOne,
+        create,
+        edit,
+        addComment,
+        delete: deleteGame,
+    };
+}
