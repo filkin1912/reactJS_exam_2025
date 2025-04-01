88d04062dce632a656bf063244a689c7b5af0945
Filkin1912 <1912filkin@gmail.com>
2025-04-02 01:39:04 +0300

    Added Bought Games Option

--- src/App.js
+++ src/App.js
@@ -1,43 +1,50 @@
 import {Routes, Route,} from "react-router-dom";
 import {GameProvider} from "./context/GameContext";
 import {AuthProvider} from "./context/AuthContext";
+import {BoughtGamesProvider} from "./context/BoughtGamescontext";
+
+import {Header} from "./components/Basic/Header/Header";
+import {Footer} from "./components/Basic/Footer/Footer";
+import {Home} from "./components/Home/Home";
 
 import {Register} from "./components/Register/Register";
 import {Login} from "./components/Login/Login";
 import {Logout} from "./components/Logout/Logout";
 import {UserDetails} from "./components/UserDetails/UserDetails";
 import {UserDetailsPage} from "./components/UserDetails/UserDetailsPage";
-import {Header} from "./components/Basic/Header/Header";
-import {Footer} from "./components/Basic/Footer/Footer";
-import {Home} from "./components/Home/Home";
+
 import {EditGame} from "./components/EditGame/EditGame";
 import {CreateGame} from "./components/CreateGame/CreateGame";
 import {Catalog} from "./components/Catalog/Catalog";
 import {DetailsGame} from "./components/DetailsGame/DetailsGame";
+import {BoughtGames} from "./components/BoughtGames/BoughtGames";
 
 function App() {
 
     return (
         <AuthProvider>
             <GameProvider>
-                <div>
-                    <Header/>
+                <BoughtGamesProvider>
+                    <div>
+                        <Header/>
 
-                    <Routes>
-                        <Route path="/" element={<Home/>}/>
-                        <Route path="/catalog" element={<Catalog/>}/>
-                        <Route path='/catalog/:gameId' element={<DetailsGame/>}/>
-                        <Route path="/catalog/:gameId/edit" element={<EditGame/>}/>
-                        <Route path="/create" element={<CreateGame/>}/>
-                        <Route path="/details/:userId" element={<UserDetailsPage/>}/>
-                        <Route path="/user-details" element={<UserDetails/>}/>
-                        <Route path="/register" element={<Register/>}/>
-                        <Route path="/logout" element={<Logout/>}/>
-                        <Route path="/login" element={<Login/>}/>
-                    </Routes>
+                        <Routes>
+                            <Route path="/" element={<Home/>}/>
+                            <Route path="/catalog" element={<Catalog/>}/>
+                            <Route path='/catalog/:gameId' element={<DetailsGame/>}/>
+                            <Route path="/catalog/:gameId/edit" element={<EditGame/>}/>
+                            <Route path="/create" element={<CreateGame/>}/>
+                            <Route path="/bought-games" element={<BoughtGames />}/>
+                            <Route path="/details/:userId" element={<UserDetailsPage/>}/>
+                            <Route path="/user-details" element={<UserDetails/>}/>
+                            <Route path="/register" element={<Register/>}/>
+                            <Route path="/logout" element={<Logout/>}/>
+                            <Route path="/login" element={<Login/>}/>
+                        </Routes>
 
-                    <Footer/>
-                </div>
+                        <Footer/>
+                    </div>
+                </BoughtGamesProvider>
             </GameProvider>
         </AuthProvider>
 
--- src/components/Catalog/Catalog.js
+++ src/components/Catalog/Catalog.js
@@ -1,17 +1,19 @@
 import { Game } from "./Game/Game";
 import {useGameContext} from "../../context/GameContext";
+import {useBoughtGamesContext} from "../../context/BoughtGamescontext";
 
 export const Catalog = () => {
     const {games} = useGameContext();
     const {filteredGames} = useGameContext();
     const gamesToDisplay = filteredGames.length > 0 ? filteredGames : games;
 
+    const {buyGame} = useBoughtGamesContext();
 
     return (
         <section id="catalog-page">
             <h1>All Games</h1>
             {
-            gamesToDisplay.map(x => <Game key={x._id} {...x} onBuy={''} />)
+            gamesToDisplay.map(x => <Game key={x._id} {...x} onBuy={buyGame} />)
             }
         </section>
     );
